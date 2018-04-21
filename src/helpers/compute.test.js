fc512e33e18e1149711a80bfa5e94a7fcc2ba663
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-04-21 15:43:31 +0200

    Test computed answers

--- src/helpers/compute.test.js
+++ src/helpers/compute.test.js
@@ -4,9 +4,20 @@ const {
   computeAnswers,
 } = require( './compute' );
 
-describe( 'helpers/questions', () => {
+describe( 'helpers/compute', () => {
   it( 'generates questions', () => {
     const generated = generateQuestions( 2 );
     expect( generated.length ).to.equal( 5 );
+  } );
+
+  it( 'computes correct answers', () => {
+    const answers = {
+      'plateau': '5 5',
+      'rover1Landing': '1 2 S',
+      'rover1Instructions': 'LMLMLMLMM'
+    };
+
+    const result = computeAnswers( answers, 1 );
+    expect( result ).to.deep.equal( { Rover1: '1 1 S' } );
   } );
 } );
