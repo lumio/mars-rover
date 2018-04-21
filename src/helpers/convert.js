9bc76d18dbbcdc8298de89a7bcb3ffeaf7694390
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-04-21 12:40:59 +0200

    Add basic function descriptions

--- src/helpers/convert.js
+++ src/helpers/convert.js
@@ -1,6 +1,9 @@
 const possibleValues = [ 'N', 'E', 'S', 'W' ];
 const possibleValuesLen = possibleValues.length;
 
+/**
+ * Convert degree to a cardinal index
+ */
 const degToDirectionKey = ( deg ) => {
   if ( deg < 0 || deg > 360 ) {
     throw new Error( 'Invalid degree. Only numbers between 0 and 360 are allowed' );
@@ -13,6 +16,9 @@ const degToDirectionKey = ( deg ) => {
   return normalized;
 }
 
+/**
+ * Convert degree to cardinal direction
+ */
 const degToDirection = ( deg ) => {
   const directionKey = degToDirectionKey( deg );
   return possibleValues[ directionKey ];
